fa6bc26b8433b727dc6ef4b3cd25d41632ef1174
Solomon Ponzio <91683225+sponzio23@users.noreply.github.com>
2021-10-07 09:50:05 -0400

    Work for 10/7
    - added a computer shot method. currently the computer just shoots completely randomly
    - added a method that prints any 2d array passed into it
    - made board length and height as well as number of ships and shots customizable by player input
    - fixed an error if the player entered a negative integer
    - attempted to fix an infinite loop if the number of spaces on the board is less than the number of shots or ships, still a wip

--- battleship.java
+++ battleship.java
@@ -5,90 +5,104 @@ import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class battleship {
-    // these three variables can all be customized to any value
-    public static int numRows = 5;
-    public static int numCols = 5;
-    public static int numShips = 3;
-    public static int numShots = 2;
+    public static int numRows;
+    public static int numCols;
+    public static int numShips;
+    public static int numShots;
 
-    // create fields for visible boards and hidden boards with ships
-    public static int[][] playerBoard = new int[numCols][numRows];
-    public static int[][] playerShips = new int[numCols][numRows];
-    public static int[][] compBoard = new int[numCols][numRows];
-    public static int[][] compShips = new int[numCols][numRows];
-
-    // create scanners to detect input for rules and ship placements
+    public static Scanner varScanner = new Scanner(System.in);
     public static Scanner rulesScanner = new Scanner(System.in);
     public static Scanner placeScanner = new Scanner(System.in);
     public static Scanner shotScanner = new Scanner(System.in);
 
+    public static int[][] playerBoard;
+    public static int[][] playerShips;
+    public static int[][] compBoard;
+    public static int[][] compShips;
+
     public static void main(String[] args) {
+        setVars();
+        newline();
+
         rules();
         newline();
 
         placeCompShips();
+        newline();
+
         placePlayerShips();
         newline();
 
-        printPlayerShips();
-        printCompShips();
-        newline();
-
-        printCompBoard();
-        printPlayerBoard();
-        newline();
-
         playerShot();
+        newline();
+
         compShot();
+        newline();
+
+        printArray(playerShips);
     }
 
-    static void newline(){
+    static void newline() {
         System.out.print("\n");
     }
 
-    static void rules(){
+    static void printArray(int[][] input){
+        for (int[] row : input) {
+            System.out.println(Arrays.toString(row));
+        }
+    }
+
+    static void setVars(){
+        System.out.println("How many rows would you like the board to have?");
+        while (!varScanner.hasNextInt()) {
+            System.out.println("Please enter a number");
+            varScanner.next();
+        }
+        numRows = Math.abs(varScanner.nextInt());
+
+        System.out.println("How many columns would you like the board to have?");
+        while (!varScanner.hasNextInt()){
+            System.out.println("Please enter a number");
+            varScanner.next();
+        }
+        numCols = Math.abs(varScanner.nextInt());
+
+        System.out.println("How many ships would you like each player to have?");
+        while (!varScanner.hasNextInt()) { // add something along the lines of
+            // && varScanner.nextInt() > (numCols * numRows)
+            System.out.println("Please enter a number less than the number of spaces on the board");
+            varScanner.next();
+        }
+        numShips = Math.abs(varScanner.nextInt());
+
+        System.out.println("How many shots would you like each player to get each round?");
+        while (!varScanner.hasNextInt()) { // add something along the lines of
+            // && varScanner.nextInt() > (numCols * numRows)
+            System.out.println("Please enter a number less than the number of spaces on the board");
+            varScanner.next();
+        }
+        numShots = Math.abs(varScanner.nextInt());
+
+
+        playerBoard = new int[numRows][numCols];
+        playerShips = new int[numRows][numCols];
+        compBoard = new int[numRows][numCols];
+        compShips = new int[numRows][numCols];
+    }
+
+    static void rules() {
         boolean rulesDone = false;
         System.out.println("Would you like to hear the rules? y/n"); // prompt for input
-        while (!rulesDone){
+        while (!rulesDone) {
             String printRules = rulesScanner.nextLine(); // reads user input
             if (printRules.equals("y")) {
                 System.out.println("RULES PLACEHOLDER");
                 rulesDone = true;
-            }
-            else if (printRules.equals("n")){
+            } else if (printRules.equals("n")) {
                 rulesDone = true;
-            }
-            else{
+            } else {
                 System.out.println("Invalid input, please try again. Respond with y or n ONLY.");
             }
-        }
-    }
-
-    static void printPlayerBoard(){
-        System.out.println("Player Board:");
-        for (int[] row : playerBoard) {
-            System.out.println(Arrays.toString(row));
-        }
-    }
-
-    static void printCompBoard(){
-        System.out.println("Computer Board:");
-        for (int[] row : compBoard) {
-            System.out.println(Arrays.toString(row));
-        }
-    }
-
-    static void printPlayerShips(){
-        System.out.println("Player ships:");
-        for (int[] row : playerShips) {
-            System.out.println(Arrays.toString(row));
-        }
-    }
-
-    static void printCompShips(){
-        System.out.println("Computer ships:");
-        for (int[] row : compShips) {
-            System.out.println(Arrays.toString(row));
         }
     }
 
@@ -112,32 +126,30 @@ public class battleship {
             newline();
             System.out.println("Place ship #" + (i + 1) + " by entering a row. This value must be between 0 and "
                     + (numRows - 1) + ".");
-            while (!placeScanner.hasNextInt()){
+            while (!placeScanner.hasNextInt()) {
                 System.out.println("Please enter a number.");
                 placeScanner.next();
             }
-            int shipRow = placeScanner.nextInt();
+            int shipRow = Math.abs(placeScanner.nextInt());
 
             System.out.println("Place ship #" + (i + 1) + " by entering a column. This value must be between 0 and "
                     + (numCols - 1) + ".");
-            while (!placeScanner.hasNextInt()){
+            while (!placeScanner.hasNextInt()) {
                 System.out.println("Please enter a number.");
                 placeScanner.next();
             }
-            int shipCol = placeScanner.nextInt();
+            int shipCol = Math.abs(placeScanner.nextInt());
             newline();
             if (shipCol > (numCols - 1) || shipRow > (numRows - 1)) {
                 System.out.println("Failed to place ship. One or both of your values is/are either too large " +
                         "or small. Please try again.");
                 i--;
-            }
-            else if (playerShips[shipRow][shipCol] == 1){
+            } else if (playerShips[shipRow][shipCol] == 1) {
                 System.out.println("Failed to place ship. That location already has a ship. Please try again.");
                 i--;
-            }
-            else {
+            } else {
                 playerShips[shipRow][shipCol] = 1;
-                System.out.println("Successfully placed ship #" + (i+1) + " at (" + shipRow + ", " + shipCol + ")");
+                System.out.println("Successfully placed ship #" + (i + 1) + " at (" + shipRow + ", " + shipCol + ")");
             }
         }
     }
@@ -152,7 +164,7 @@ public class battleship {
                 System.out.println("Please enter a number.");
                 shotScanner.next();
             }
-            int shotRow = shotScanner.nextInt();
+            int shotRow = Math.abs(shotScanner.nextInt());
 
             System.out.println("Take shot #" + (i + 1) + " by entering a column. This value must be between 0 and "
                     + (numCols - 1) + ".");
@@ -160,14 +172,14 @@ public class battleship {
                 System.out.println("Please enter a number.");
                 shotScanner.next();
             }
-            int shotCol = shotScanner.nextInt();
+            int shotCol = Math.abs(shotScanner.nextInt());
             newline();
             if (shotCol > (numCols - 1) || shotRow > (numRows - 1)) {
                 System.out.println("Failed to take shot. One or both of your values is/are either too large " +
                         "or small. Please try again.");
                 newline();
                 i--;
-            } else if (compShips[shotRow][shotCol] == 2) {
+            } else if (playerShips[shotRow][shotCol] == 2 || playerShips[shotRow][shotCol] == 3) {
                 System.out.println("Failed to take shot. That location has already been shot at. Please try again.");
                 i--;
                 newline();
@@ -177,8 +189,7 @@ public class battleship {
                     System.out.println("HIT!!!");
                     compShips[shotRow][shotCol] = 3;
                     playerBoard[shotRow][shotCol] = 3;
-                }
-                else {
+                } else {
                     compShips[shotRow][shotCol] = 2;
                     playerBoard[shotRow][shotCol] = 2;
                     System.out.println("You missed, sorry.");
@@ -187,8 +198,27 @@ public class battleship {
         }
     }
 
-    // comp shot is wip
-    static void compShot(){
-        System.out.println("pew pew");
+    static void compShot() {
+        System.out.println("It's the computer's turn to shoot!");
+        for (int i = 0; i < numShots; i++) {
+            int shotRow = ThreadLocalRandom.current().nextInt(0, numRows);
+            int shotCol = ThreadLocalRandom.current().nextInt(0, numCols);
+            if (playerShips[shotRow][shotCol] == 2 || playerShips[shotRow][shotCol] == 3) {
+                System.out.println("Shot failed.");
+                i--;
+                newline();
+            } else {
+                System.out.println("The computer fired shot #" + (i + 1) + " at (" + shotRow + ", " + shotCol + ")");
+                if (playerShips[shotRow][shotCol] == 1) {
+                    System.out.println("HIT!!!");
+                    playerShips[shotRow][shotCol] = 3;
+                    compBoard[shotRow][shotCol] = 3;
+                } else {
+                    playerShips[shotRow][shotCol] = 2;
+                    compBoard[shotRow][shotCol] = 2;
+                    System.out.println("The computer missed!");
+                }
+            }
+        }
     }
 }
